1dbefb155c73bd079175e1bb6997c380c1d83453
Sarat Kollimarla <Sarat@ACE-1224.fios-router.home>
2019-02-23 16:13:28 -0500

    initial commit

---
+++ data.js
@@ -0,0 +1,362 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-7_bed",
+      "name": "7_bed",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.9820936234864455,
+        "pitch": 0.055946244496798414,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5030603697501883,
+          "pitch": 0.48049124030531587,
+          "rotation": 0,
+          "target": "1-8_closet"
+        },
+        {
+          "yaw": -1.792512921918103,
+          "pitch": 0.7009159494286425,
+          "rotation": 0,
+          "target": "2-6_couch"
+        },
+        {
+          "yaw": -2.262881010214139,
+          "pitch": 0.3830298121781226,
+          "rotation": 0,
+          "target": "4-4_livingroom"
+        },
+        {
+          "yaw": -1.5009095349395665,
+          "pitch": 0.348887064437875,
+          "rotation": 1.5707963267948966,
+          "target": "3-5_workspace"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-8_closet",
+      "name": "8_closet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-6_couch",
+      "name": "6_couch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.2274319684409996,
+        "pitch": 0.024569408194899722,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5342732716176712,
+          "pitch": 0.5598931768668116,
+          "rotation": 0,
+          "target": "4-4_livingroom"
+        },
+        {
+          "yaw": 1.7409227415093635,
+          "pitch": 0.5930514320956011,
+          "rotation": 0.7853981633974483,
+          "target": "3-5_workspace"
+        },
+        {
+          "yaw": -0.13884087381814503,
+          "pitch": 0.37666268917887,
+          "rotation": 4.71238898038469,
+          "target": "5-3_kitchen"
+        },
+        {
+          "yaw": -2.113902652053712,
+          "pitch": 0.7836031266388197,
+          "rotation": 0,
+          "target": "0-7_bed"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-5_workspace",
+      "name": "5_workspace",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.07078276575288278,
+        "pitch": 0.04596477801053389,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2124220448740903,
+          "pitch": 0.6353086888940531,
+          "rotation": 0,
+          "target": "2-6_couch"
+        },
+        {
+          "yaw": -2.8374428489776236,
+          "pitch": 0.4863845944568741,
+          "rotation": 0,
+          "target": "4-4_livingroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-4_livingroom",
+      "name": "4_Livingroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5724501586262853,
+          "pitch": 0.6188947154963618,
+          "rotation": 0,
+          "target": "3-5_workspace"
+        },
+        {
+          "yaw": 2.0148034991961126,
+          "pitch": 0.6241192066778076,
+          "rotation": 0.7853981633974483,
+          "target": "5-3_kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-3_kitchen",
+      "name": "3_kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.9759446219706973,
+        "pitch": 0.30576569720054003,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3053771209708653,
+          "pitch": 0.4811112688897978,
+          "rotation": 0,
+          "target": "4-4_livingroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2_bathroom",
+      "name": "2_bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.4771682006291726,
+        "pitch": 0.19378141153620732,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.691182936039457,
+          "pitch": 0.5112925846465703,
+          "rotation": 0,
+          "target": "7-1_foyer"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-1_foyer",
+      "name": "1_Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.15578920648745864,
+          "pitch": 0.4218782238021994,
+          "rotation": 0.7853981633974483,
+          "target": "6-2_bathroom"
+        },
+        {
+          "yaw": -1.3647577823070876,
+          "pitch": 0.36262629539645985,
+          "rotation": 0,
+          "target": "5-3_kitchen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
